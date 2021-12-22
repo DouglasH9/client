5692a38db91cb03492d489a7def3fdd19c61559d
Doug Herman <douglash9882@gmail.com>
2021-12-22 00:42:41 -0600

    asdfasdf

---
+++ src/Components/RegisterForm.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+import axios from "axios";
+
+const RegisterForm = (props) => {
+
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordCon, setPasswordCon] = useState("");
+}
